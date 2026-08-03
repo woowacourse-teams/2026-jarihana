c416578009c492a576a6fdc0e65deff98edb847f
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-08-03 13:28:07 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/com/project/jarihana/JarihanaApplication.java
@@ -0,0 +1,13 @@
+package com.project.jarihana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarihanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarihanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/jarihana/JarihanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jarihana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarihanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
